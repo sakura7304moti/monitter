c01ffa39029d86e5c15eadf34bada64a041937dd
sakura7304moti <sakura7304moti@gmail.com>
2023-08-02 21:11:56 +0900

    やれやれだぜ

---
+++ src/moti.py
@@ -0,0 +1,18 @@
+from . import const,utils,sqlite
+from tqdm import tqdm
+holoList = const.holoList()
+
+class Scraper:
+    def base_scraper(self, query: str, date: int = 30, limit: int = 3000):
+        driver = utils.get_driver()
+        df = utils.get_tweet(query,limit,date,driver)
+        sqlite.update(df,query,'base')
+        driver.close()
+        
+    def holo_scraper(self, date: int = 30, limit: int = 3000):
+        driver = utils.get_driver()
+        for query in tqdm(holoList,desc="holo scraper"):
+            print(f'Fanart : {query}')
+            df = utils.get_tweet(query,limit,date,driver)
+            sqlite.update(df,query,'holo')
+        driver.close()
