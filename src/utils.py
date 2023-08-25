fd28a0b9179c4ce94a578bd3783b3a67f6e7648b
sakura7304moti <sakura7304moti@gmail.com>
2023-08-25 21:14:10 +0900

    user-agent

--- src/utils.py
+++ src/utils.py
@@ -48,6 +48,7 @@ def get_driver(headless=True):
         print("Scraping on headless mode.")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")  # An error will occur without this line
+        options.add_argument('--user-agent=' + 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36')#user-agent
         options.headless = True
     else:
         options.headless = False
